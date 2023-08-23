7a8f646dad60975b4417a73a5cb2f670b465484f
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-23 14:10:56 +0900

    ddit 고급자바 0823

--- collectionTest/src/kr/or/ddit/basic/ArrayListTest02.java
+++ collectionTest/src/kr/or/ddit/basic/ArrayListTest02.java
@@ -1,49 +1,35 @@
 package kr.or.ddit.basic;
+/*
+문제) 5명의 사람 이름을 입력 받아 ArrayList에 추가한 후에
+	  ArrayList에 저장된 이름들 중에 '김'씨 성의 이름을 모두 출력하시오.
+	  (단, 입력은 Scanner객체를 이용)
+*/
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
- 문제) 5명의 사람 이름을 입력 받아 ArrayList에 추가한 후에
- 	  ArrayList에 저장된 이름들 중에 '김'씨 성의 이름을 모두 출력하시오.
- 	  (단, 입력은 Scanner객체를 이용)
- */
-
 public class ArrayListTest02 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        //이름 입력받기
+        ArrayList<String> names = new ArrayList<>(); 
+        //이름 입력 -> String
 
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("김민채");
-		list.add("박상협");
-		list.add("한동욱");
-		list.add("전민균");
-		list.add("정소현");
-		
-		String[] names = new String[5];
-		
-		Scanner sc = new Scanner(System.in);
-		
-		for(int i=0; i<5; i++) {
-			System.out.print(i+"번째 이름 입력: ");
-			names[i] = sc.nextLine();
-		}
-		
+        System.out.println("***5명의 이름을 입력하세요!***");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i+"번째 이름 입력: ");
+            String name = sc.nextLine().trim();
+            names.add(name);
+        }
 
-
-		
-
-		
-		
-//		ArrayList<String> list = new ArrayList<String>();
-		
-		
-
-		
-
-	}
-
-
-	}
-
-
+        System.out.print("김씨 성을 가진 이름들:");
+        //첫번째 글자가 '김'인 사람을 출력
+        for (String name : names) {
+            int index = name.indexOf("김");
+            if (index != -1) {
+                System.out.print(name+"|");
+            }
+        }
+    }
+}
---
+++ collectionTest/src/kr/or/ddit/basic/ArrayListTest02Teaching.java
@@ -0,0 +1,105 @@
+package kr.or.ddit.basic;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListTest02Teaching {
+
+	public static void main(String[] args) {
+		method01();
+		method02();
+		method03();
+		method04();
+
+	}
+	public static void method01() {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> nameList = new ArrayList<String>();
+
+		System.out.println("5명의 이름을 입력하세요!");
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i + "번째 사람 이름>>");
+			String name = sc.nextLine();
+			nameList.add(name);
+		}
+
+		System.out.println();
+
+		System.out.println("김씨 성을 가진 사람들..");
+		for (int i = 0; i < nameList.size(); i++) {
+			if (nameList.get(i).substring(0, 1).equals("김")) {
+				System.out.println(nameList.get(i));
+			}
+		}
+
+	}
+	
+	public static void method02() {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> nameList = new ArrayList<String>();
+
+		System.out.println("5명의 이름을 입력하세요!");
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i + "번째 사람 이름>>");
+			String name = sc.nextLine();
+			nameList.add(name);
+		}
+
+		System.out.println();
+
+		System.out.println("김씨 성을 가진 사람들..");
+		for (int i = 0; i < nameList.size(); i++) {
+			if(nameList.get(i).charAt(0)=='김') {
+				System.out.println(nameList.get(i));
+			}
+		}
+
+	}
+	
+	public static void method03() {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> nameList = new ArrayList<String>();
+
+		System.out.println("5명의 이름을 입력하세요!");
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i + "번째 사람 이름>>");
+			String name = sc.nextLine();
+			nameList.add(name);
+		}
+
+		System.out.println();
+
+		System.out.println("김씨 성을 가진 사람들..");
+		for (int i = 0; i < nameList.size(); i++) {
+			if(nameList.get(i).startsWith("김")) {
+				System.out.println(nameList.get(i));
+			}
+		}
+
+	}
+	
+	public static void method04() {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> nameList = new ArrayList<String>();
+
+		System.out.println("5명의 이름을 입력하세요!");
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i + "번째 사람 이름>>");
+			String name = sc.nextLine();
+			nameList.add(name);
+		}
+
+		System.out.println();
+
+		System.out.println("김씨 성을 가진 사람들..");
+		for (int i = 0; i < nameList.size(); i++) {
+			if(nameList.get(i).indexOf("김")==0){
+				System.out.println(nameList.get(i));
+			}
+		}
+
+	}
+	
+	
+
+}
