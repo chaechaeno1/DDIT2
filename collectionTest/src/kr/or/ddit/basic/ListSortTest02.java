56160fcabee26c921de037b2ead670e1bbd53150
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-23 18:58:16 +0900

    0823

--- collectionTest/src/kr/or/ddit/basic/ListSortTest02.java
+++ collectionTest/src/kr/or/ddit/basic/ListSortTest02.java
@@ -1,6 +1,8 @@
 package kr.or.ddit.basic;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ListSortTest02 {
 
@@ -20,9 +22,17 @@ public class ListSortTest02 {
 		}
 		System.out.println("============================================");
 		
+		
 		//Member의 num값의 내림차순으로 정렬하는 외부 정렬 기준 클래스를 이용하여 정렬하시오.
 		//(클래스명 : SortNumDesc) ->> 하단에 클래스 추가
 		//<Member>
+		
+		Collections.sort(memList,new SortNumDesc());
+		
+		System.out.println("정렬 후...");
+		for(Member mem : memList) {
+			System.out.println(mem);
+		}
 		
 	}
 
@@ -70,4 +80,17 @@ class Member {
 		return "Member [num=" + num + ", name=" + name + ", tel=" + tel + "]";
 	}
 
+}
+
+//Member의 num값의 "내림차순"으로 정렬하는 외부 정렬 기준 클래스
+class SortNumDesc implements Comparator<Member>{
+	
+	@Override
+	public int compare(Member mem1, Member mem2) {
+		return Integer.compare(mem2.getNum(), mem1.getNum());
+		//Integer.compare(a, b)는 두 개의 정수를 비교하여 비교 결과를 반환하는 정적 메서드
+		//내림차순 정렬을 위해 (mem2.getNum(), mem1.getNum())
+		//mem2가 앞으로 가도록 내림차순 정렬
+	}
+	
 }
