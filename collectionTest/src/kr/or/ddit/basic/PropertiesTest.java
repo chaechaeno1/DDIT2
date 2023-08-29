fd804581c84ca9b6d4db107a8628470c331b4590
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-29 15:00:53 +0900

    0829

---
+++ collectionTest/src/kr/or/ddit/basic/PropertiesTest.java
@@ -0,0 +1,26 @@
+package kr.or.ddit.basic;
+
+public class PropertiesTest {
+
+	public static void main(String[] args) {
+	/*
+	 	Properties 객체
+	 	- Properties는 Map보다 축소된 기능의 객체라고 할 수 있다.
+	 	
+	 	- Map은 key값과 value값에 모든 종류의 객체를 사용할 수 있지만
+	 	Properties는 key값과 value값에 String만 사용할 수 있다.
+	 	
+	 	- Map은 put(), get()메서드를 이용하여 데이터를 입출력하지만
+	 	Properties는 setProperty(), getProperty()메서드를 통해서 데이터를 입출력 한다.
+	 	
+	 	- Properties는 데이터를 파일로 입출력하는 기능이 있다.
+	 	
+	 	
+	 	
+	 	
+	 	
+	 */
+
+	}
+
+}
