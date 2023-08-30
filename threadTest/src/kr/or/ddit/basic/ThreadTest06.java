1c868a6a0a171dfa00b1a86b8f559c71eaa59e64
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-30 17:18:34 +0900

    0830

--- threadTest/src/kr/or/ddit/basic/ThreadTest06.java
+++ threadTest/src/kr/or/ddit/basic/ThreadTest06.java
@@ -5,14 +5,13 @@ import javax.swing.JOptionPane;
 public class ThreadTest06 {
 
 	public static void main(String[] args) {
-		
+
 		Thread th1 = new DataInput();
 		Thread th2 = new CountDown();
 
 		th1.start();
 		th2.start();
-		
-		
+
 	}
 
 }
@@ -20,9 +19,14 @@ public class ThreadTest06 {
 // 데이터를 입력하는 쓰레드 클래스
 
 class DataInput extends Thread {
+	// 입력 완료 여부를 확인하기 위한 변수 선언(쓰레드에서 공통으로 사용할 변수)
+	// static으로 선언하면 다른 쓰레드에서도 사용가능
+	public static boolean inputCheck = false;
+
 	@Override
 	public void run() {
 		String str = JOptionPane.showInputDialog("아무거나 입력하세요!");
+		inputCheck = true; // 입력이 완료되면 inputCheck 변수를 true로 변경
 		System.out.println("입력한 값 : " + str);
 	}
 
@@ -33,6 +37,11 @@ class CountDown extends Thread {
 	@Override
 	public void run() {
 		for (int i = 10; i >= 1; i--) {
+			// 입력 완료 여부 검사
+			// ==> 입력이 완료되면 쓰레드를 종료시킴
+			if (DataInput.inputCheck == true) {
+				return; // run()메서드가 종료되면 쓰레드도 종료.
+			}
 			System.out.println(i);
 			try {
 				Thread.sleep(1000);
@@ -40,6 +49,6 @@ class CountDown extends Thread {
 			}
 		}
 		System.out.println("10초가 지났습니다. 프로그램을 종료합니다.");
-		System.exit(0); //프로그램 강제종료
+		System.exit(0); // 프로그램 강제종료
 	}
 }
