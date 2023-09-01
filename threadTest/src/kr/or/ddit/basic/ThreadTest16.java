d422f7c5c47a5020ff007924dd165f32f81fdb5f
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-09-01 17:39:29 +0900

    0901

--- threadTest/src/kr/or/ddit/basic/ThreadTest16.java
+++ threadTest/src/kr/or/ddit/basic/ThreadTest16.java
@@ -1,6 +1,36 @@
 package kr.or.ddit.basic;
 
-public class ThreadTest16 {
+//은행의 입출금을 쓰레드로 처리하는 예제
+
+public class ThreadTest16 { // 공통 객체
+	private int balance; //잔액이 저장될 변수
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance=balance;
+	}
+	
+	//입금을 처리하는 메서드
+	public void deposit(int money) {
+		balance += money;
+	}
+	
+	//출금을 처리하는 메서드(반환값 : 출금 성공 true, 출금 실패 false)
+	public boolean withdraw(int money) {
+		if(balance >= money) {
+			for(int i=1; i<=10000000; i++) { //출력 시간을 지연하기 위한 코드
+				int a = i + 3;
+			}
+			balance -= money;
+			System.out.println("메서드 안에서 balance = "+balance);
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
