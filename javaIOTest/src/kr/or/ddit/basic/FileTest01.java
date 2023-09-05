e33e76c8d42f663ec1c1bd3f2674c4c31912e6f9
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-09-05 14:22:30 +0900

    0905

---
+++ javaIOTest/src/kr/or/ddit/basic/FileTest01.java
@@ -0,0 +1,22 @@
+package kr.or.ddit.basic;
+
+import java.io.File;
+
+public class FileTest01 {
+
+	public static void main(String[] args) {
+		//File객체 만들기 연습
+		
+		//1. new File(String 파일 또는 경로)
+		//==> 디렉토리와 디렉토리 사이 도는 디렉토리와 파일명 사이의 
+		//	  구분 문자는 '/'를 사용하거나 '\'를 사용할 수 있다.
+		
+		
+//		File file1 = new File("d:/d_other/test.txt"); // 구분 문자를 '/'로 사용
+		File file1 = new File("d:\\d_other\\test.txt"); // 구분 문자를 '\'로 사용
+		
+		
+		
+	}
+
+}
