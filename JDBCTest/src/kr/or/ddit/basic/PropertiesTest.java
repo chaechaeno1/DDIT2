0dcadb5f56258c6ca14ab5547d85556cf9d67765
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-09-15 16:08:55 +0900

    0915

--- JDBCTest/src/kr/or/ddit/basic/PropertiesTest.java
+++ JDBCTest/src/kr/or/ddit/basic/PropertiesTest.java
@@ -1,9 +1,14 @@
 package kr.or.ddit.basic;
 
+import java.util.Properties;
+
 public class PropertiesTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 읽어온 파일 정보를 저장할 Properties객체 생성
+		Properties prop = new Properties();
+		
+		// 읽어올 파일 정보 
 
 	}
 
