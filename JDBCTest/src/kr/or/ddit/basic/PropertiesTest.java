6a5236521c7afcd483d8fb8971f4f671adce696a
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-09-15 16:14:36 +0900

    0915

--- JDBCTest/src/kr/or/ddit/basic/PropertiesTest.java
+++ JDBCTest/src/kr/or/ddit/basic/PropertiesTest.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.basic;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertiesTest {
@@ -8,8 +10,29 @@ public class PropertiesTest {
 		// 읽어온 파일 정보를 저장할 Properties객체 생성
 		Properties prop = new Properties();
 		
-		// 읽어올 파일 정보 
-
+		// 읽어올 파일 정보를 이용한 File객체 생성
+		File f = new File("res/kr/or/ddit/jdbc/config/dbinfo.properties");
+		FileInputStream fin = null;
+		
+		try {
+			//파일 내용을 읽어올 스트림 객체 생성
+			fin = new FileInputStream(f);
+			
+			//입력 스트림을 이용하여 파일 내용을 읽어와 Properties 객체에 저장하기
+			
+			prop.load(fin); // 파일 내용을 읽어와 key값과 value값을 분류한 후
+							// 분류된 자료를 Properties객체에 추가한다.
+			
+			//읽어온 정보 출력해보기
+			System.out.println("driver : "+prop.getProperty("driver"));
+			System.out.println("driver : "+prop.getProperty("url"));
+			System.out.println("driver : "+prop.getProperty("user"));
+			System.out.println("driver : "+prop.getProperty("pass"));
+			
+			
+		} catch (Exception e) {
+			
+		}
 	}
 
 }
