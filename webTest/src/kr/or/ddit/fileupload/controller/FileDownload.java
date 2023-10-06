a6cbed25374e4b0f066220fd0143179019d73926
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-06 16:42:41 +0900

    1006

--- webTest/src/kr/or/ddit/fileupload/controller/FileDownload.java
+++ webTest/src/kr/or/ddit/fileupload/controller/FileDownload.java
@@ -7,17 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FileDownload
- */
+//파일 다운로드를 처리하는 서블릿
 @WebServlet("/fileDownload.do")
 public class FileDownload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	
